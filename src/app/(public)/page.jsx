5968e1e072e49eaa04ed5f40a95fed225fe14ab1
top0329 <top000329@gmail.com>
2024-09-03 21:13:19 +1000

    fix: landing page update

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -57,10 +57,11 @@ export default function Home() {
           <div className="col-lg-6">
             <div className="cs-height_0 cs-height_lg_40" />
             <Div className={`cs-section_heading cs-style1`}>
-              <h2 className="cs-section_title">Step 1.</h2>
-              <h3 className="cs-section_subtitle">
+              <h2 className="cs-section_title">Step 1</h2>
+              <br />
+              <p className="cs-section_subtitle" style={{ textIndent: "15px" }}>
                 {`Upload your job description and resume, and let our AI create unlimited, personalized interview questions based on your experience and the role you want. It’s like having a coach who knows exactly what you need to practice.`}
-              </h3>
+              </p>
             </Div>
           </div>
           <div className="col-lg-6">
@@ -76,10 +77,11 @@ export default function Home() {
         <div className="row flex-lg-row-reverse align-items-center">
           <div className="col-lg-6">
             <Div className={`cs-section_heading cs-style1`}>
-              <h2 className="cs-section_title">Step 2.</h2>
-              <h3 className="cs-section_subtitle">
+              <h2 className="cs-section_title">Step 2</h2>
+              <br />
+              <p className="cs-section_subtitle" style={{ textIndent: "15px" }}>
                 {`Struggling with an answer? No worries, our AI can generate one based on your experience. Need to polish it? Just tell us how, and we’ll rewrite it for you.`}
-              </h3>
+              </p>
             </Div>
           </div>
           <div className="col-lg-6">
@@ -95,10 +97,11 @@ export default function Home() {
         <div className="row align-items-center">
           <div className="col-lg-6">
             <Div className={`cs-section_heading cs-style1`}>
-              <h2 className="cs-section_title">Step 3.</h2>
-              <h3 className="cs-section_subtitle">
+              <h2 className="cs-section_title">Step 3</h2>
+              <br />
+              <p className="cs-section_subtitle" style={{ textIndent: "15px" }}>
                 {`Get an interview experience with our AI-powered mock interviewer. Answer questions out loud while our AI records your responses, so you can see and hear exactly how you perform. Afterward, review the recording and receive detailed feedback to help you refine your answers and presentation.`}
-              </h3>
+              </p>
             </Div>
           </div>
           <div className="col-lg-6">
